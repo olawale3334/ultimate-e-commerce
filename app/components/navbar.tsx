795242d77fff9a53067eb688b9b28b7a3629f564
olawale3334 <olawale3334@gmail.com>
2025-11-29 21:06:27 +0100

    nav bar item added and intall react icons

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import { Package, Heart, ShoppingCart, User } from "lucide-react";
 
 function Navbar() {
   return (
     <div>
-        <div>
+        <div className="flex justify-between items-center  bg-white p-5 shadow-md">
             <div>
                 <span>Ultimate</span>
             </div>
-            <div>
-                <p>Orders</p>
-                <p>Favourites</p>
-                <p>Cart</p>
-                <p>profile</p>
+            <div className=" flex items-center gap-3">
+                  <p className="flex gap-2 items-center">
+                     <Package/>
+                     Orders
+                  </p>
+                <p className="flex gap-2 items-center"> <Heart/>  Favourites</p>
+                <p className="flex gap-2 items-center"> <ShoppingCart/> Cart</p>
+                <p className="flex gap-2 items-center"> <User/> profile</p>
             </div>
         </div>
     </div>
