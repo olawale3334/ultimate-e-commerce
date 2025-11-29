d618dfab9b75d4716dc7c6b4015d020f82614b19
olawale3334 <olawale3334@gmail.com>
2025-11-29 20:26:09 +0100

    setting up my project

---
+++ app/components/navbar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <div>
+            <div>
+                <span>Ultimate</span>
+            </div>
+            <div>
+                <p>Orders</p>
+                <p>Favourites</p>
+                <p>Cart</p>
+                <p>profile</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
